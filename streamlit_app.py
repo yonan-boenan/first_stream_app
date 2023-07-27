77efb1698bdf98af9c183956b3ee1d4523f98a13
yonan-boenan <140401793+yonan-boenan@users.noreply.github.com>
2023-07-27 15:37:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,5 +12,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
 
